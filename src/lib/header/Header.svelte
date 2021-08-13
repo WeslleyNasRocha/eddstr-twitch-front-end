08a58bf1aa98123ff668c5f93c768418a7b94d87
Weslley Rocha <weslleytato.12@gmail.com>
2021-08-13 11:01:46 -0300

    paths

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -11,7 +11,7 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="{base}/">Home</a></li>
 			<li class:active={$page.path === '/twitch'}>
 				<a sveltekit:prefetch href="{base}/twitch">Twitch</a>
 			</li>
